d8008e542796fd957d80188bba22fff6576b2937
Dinesh Lama <dinesh99981@gmail.com>
2024-09-29 15:24:06 +0000

    test codespace commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      This is a Tailwind CSS styled component.
+      <div className='w-100px h-50px bg-blue-300'>hello</div>
+    </div>
+  );
+}
+
+export default App;
